d1804d780b8e21fe5981d89fdbbea5588862619c
dvlproad <studyroad@qq.com>
2019-03-06 18:45:05 +0800

    【Feature】实现package：adapt_cjhelper

--- adapt_cjhelper/lib/adapt_cjhelper.dart
+++ adapt_cjhelper/lib/adapt_cjhelper.dart
@@ -1,7 +1,37 @@
 library adapt_cjhelper;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+
+
+import 'package:flutter/material.dart';
+
+class AdaptCJHelper {
+  int designWidth;  /// design width
+  int designHeight; /// design height
+  static AdaptCJHelper instance = new AdaptCJHelper();
+  static AdaptCJHelper getInstance() {
+    return instance;
+  }
+  AdaptCJHelper({
+    this.designWidth = 1080,
+    this.designHeight = 1920,
+  });
+
+  static double _screenWidth;
+  static double _screenHeight;
+  void init(BuildContext context) {
+    MediaQueryData mediaQuery = MediaQuery.of(context);
+    _screenWidth = mediaQuery.size.width;
+    _screenHeight = mediaQuery.size.height;
+  }
+
+  get scaleWidth => _screenWidth / instance.designWidth;
+  get scaleHeight => _screenHeight / instance.designHeight;
+
+  setWidth(int width) => width * scaleWidth;    // result unit:dp
+  setHeight(int height) => height * scaleHeight;// result unit:dp
 }
